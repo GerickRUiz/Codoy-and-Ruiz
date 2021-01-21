0873748fc192a68e7fcc7fa08c922e88e6949df3
GerickRUiz <gerick.ruiz090208@my.lsgh.edu.ph>
2021-01-21 17:17:50 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -7,13 +7,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   gender: string;
-  height: number;
+  feet: number;
+  inches: number;
+  robinson_formula: number;
+  miller_formula: number;
+  hamwi_formula: number;
+  devine_formula: number;
+
+  robinsom_formula2: number;
+  miller_formula2: number;
+  hamwi_formula2: number;
+  devine_formula2: number;
 
 
 
 constructor(public navCtrl: NavController) {
-  this.Robinson_formula = this.height - 5 * 1.9 + 52;
+  this.robinson_formula = this.inches * 1.9 + 52;
+  this.miller_formula = this.inches * 1.41 + 56.2;
+  this.hamwi_formula = this.inches * 2.7 + 48.0;
+  this.devine_formula = this.inches * 2.3 + 50.0;
+  this.robinsom_formula2 = this.inches * 1.7 + 49;
+  this.miller_formula2 = this.inches * 1.36 + 53.1;
+  this.hamwi_formula2 = this.inches * 2.2 + 45.5;
+  this.devine_formula2 = this.inches * 2.3 + 45.5;
+
+ if (this.gender == "m") {
+   this.robinson_formula = Math.round (this.inches * 1.9 + 52);
+   this.miller_formula = Math.round (this.inches * 1.41 + 56.2);
+   this.hamwi_formula = Math.round (this.inches * 2.7 + 48.0);
+   this.devine_formula = Math.round (this.inches * 2.3 + 50.0);
   
+
+ } else if (this.gender == "f") {
+   this.robinsom_formula2 = Math.round (this.inches * 1.7 + 49);
+   this.miller_formula2
+
+
+ }
+
+
 
 
 
